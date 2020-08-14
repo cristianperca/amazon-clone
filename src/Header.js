72bcda93c1e80517bb627cf24f46b7e3dec300c3
Cristian Perca <cristianperca@yahoo.com>
2020-08-14 18:30:33 +0200

    Add navbar; Bug to solve: searchbar should take all available width

---
+++ src/Header.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function Header() {
+  return (
+    <nav className="header">
+      {/* logon on the left */}
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
+
+      {/* Search box */}
+      <div className="header__search">
+        <input type="text" className="header_searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      {/* 3 Links */}
+      <div className="header__nav">
+        {/* 1rst link */}
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello Qazi</span>
+            <span className="header__optionLineTwo">SignIn</span>
+          </div>
+        </Link>
+
+        {/* 2nd link */}
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        {/* 3rd link */}
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+        {/* Basket icon with number */}
+        {/* 4th link */}
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            {/* Shopping basket icon */}
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
